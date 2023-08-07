d50cc47a57d434db2390ec16ba79d32e1a8b9223
hector8th <115110070+hector8th@users.noreply.github.com>
2023-08-07 16:59:05 -0400

    necessary

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+document.getElementById('resume-link').onclick = function() {
+    window.open('https://hector8th.github.io/resume', "_blank");
+}
+
 function Footer() {
     return (
-        <div className='footer font'>
-            Footer
+        <div className='footer'>
+            <text className='directory'>
+                ** I do not own any Zelda or Nintendo-related content **
+            </text>
+            <p id='resume-link' className='footerFont'>
+                About Me
+            </p>
         </div>
     )
 }
